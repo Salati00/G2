56d308f1dd4880ec9e6cc871ad90a48985a4b6a0
VanyXD <54842983+VanyXD@users.noreply.github.com>
2020-09-21 23:11:08 +0200

    Added logic and database layers

---
+++ G2_HelpDesk/G2_DAL/BaseDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace G2_DAL
+{
+    public class BaseDAO
+    {
+        public void TestingDAO()
+        {
+            var client = new MongoClient("mongodb+srv://dbUser:noww6w4agyqOhr4s@g2.kxnnm.azure.mongodb.net/g2Database?retryWrites=true&w=majority");
+            var database = client.GetDatabase("g2Database");
+
+            
+        }
+    }
+}
