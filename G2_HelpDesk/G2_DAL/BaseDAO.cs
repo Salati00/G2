33f918303614f3ec590635fbfbbbb929123b32ad
VanyXD <54842983+VanyXD@users.noreply.github.com>
2020-09-22 01:02:41 +0200

    slight changes to DAO

--- G2_HelpDesk/G2_DAL/BaseDAO.cs
+++ G2_HelpDesk/G2_DAL/BaseDAO.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace G2_DAL
 {
     public class BaseDAO
     {
+        MongoClient client;
+        IMongoDatabase database;
+
         public void TestingDAO()
         {
-            var client = new MongoClient("mongodb+srv://dbUser:noww6w4agyqOhr4s@g2.kxnnm.azure.mongodb.net/g2Database?retryWrites=true&w=majority");
-            var database = client.GetDatabase("g2Database");
-
-            
+            client = new MongoClient("mongodb+srv://dbUser:noww6w4agyqOhr4s@g2.kxnnm.azure.mongodb.net/g2Database?retryWrites=true&w=majority");
+            database = client.GetDatabase("g2Database");
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("Users");
         }
     }
 }
