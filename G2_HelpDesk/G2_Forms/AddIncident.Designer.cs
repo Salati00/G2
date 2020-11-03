31dfb6f4aff0071e0ec8ac04def98b65242bc26f
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2020-11-03 16:47:13 +0100

    erjano

--- G2_HelpDesk/G2_Forms/AddIncident.Designer.cs
+++ G2_HelpDesk/G2_Forms/AddIncident.Designer.cs
@@ -38,19 +38,20 @@
             this.cbType = new System.Windows.Forms.ComboBox();
             this.cbUser = new System.Windows.Forms.ComboBox();
             this.cbPriority = new System.Windows.Forms.ComboBox();
-            this.txtSubject = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.txtDeadline = new System.Windows.Forms.TextBox();
-            this.uc_MenuBar1 = new G2_Forms.Uc_MenuBar();
             this.BTD_reportedDate = new System.Windows.Forms.DateTimePicker();
+            this.uc_MenuBar1 = new G2_Forms.Uc_MenuBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(139, 148);
+            this.label2.Location = new System.Drawing.Point(144, 191);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 1;
@@ -59,7 +60,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(139, 185);
+            this.label3.Location = new System.Drawing.Point(144, 228);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 2;
@@ -68,7 +69,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(139, 219);
+            this.label4.Location = new System.Drawing.Point(144, 262);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 13);
             this.label4.TabIndex = 3;
@@ -77,7 +78,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(139, 258);
+            this.label5.Location = new System.Drawing.Point(144, 301);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 13);
             this.label5.TabIndex = 4;
@@ -86,7 +87,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(139, 294);
+            this.label6.Location = new System.Drawing.Point(144, 337);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 13);
             this.label6.TabIndex = 5;
@@ -95,7 +96,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(139, 329);
+            this.label7.Location = new System.Drawing.Point(144, 372);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(66, 13);
             this.label7.TabIndex = 6;
@@ -103,7 +104,7 @@
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(275, 326);
+            this.txtDescription.Location = new System.Drawing.Point(280, 369);
             this.txtDescription.Multiline = true;
             this.txtDescription.Name = "txtDescription";
             this.txtDescription.Size = new System.Drawing.Size(221, 140);
@@ -112,7 +113,7 @@
             // cbType
             // 
             this.cbType.FormattingEnabled = true;
-            this.cbType.Location = new System.Drawing.Point(275, 182);
+            this.cbType.Location = new System.Drawing.Point(280, 225);
             this.cbType.Name = "cbType";
             this.cbType.Size = new System.Drawing.Size(221, 21);
             this.cbType.TabIndex = 9;
@@ -121,7 +122,7 @@
             // cbUser
             // 
             this.cbUser.FormattingEnabled = true;
-            this.cbUser.Location = new System.Drawing.Point(275, 219);
+            this.cbUser.Location = new System.Drawing.Point(280, 262);
             this.cbUser.Name = "cbUser";
             this.cbUser.Size = new System.Drawing.Size(221, 21);
             this.cbUser.TabIndex = 10;
@@ -129,24 +130,17 @@
             // cbPriority
             // 
             this.cbPriority.FormattingEnabled = true;
-            this.cbPriority.Location = new System.Drawing.Point(275, 255);
+            this.cbPriority.Location = new System.Drawing.Point(280, 298);
             this.cbPriority.Name = "cbPriority";
             this.cbPriority.Size = new System.Drawing.Size(221, 21);
             this.cbPriority.TabIndex = 11;
-            // 
-            // txtSubject
-            // 
-            this.txtSubject.Location = new System.Drawing.Point(300, 46);
-            this.txtSubject.Name = "txtSubject";
-            this.txtSubject.Size = new System.Drawing.Size(221, 20);
-            this.txtSubject.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.SystemColors.Window;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(12, 91);
+            this.label8.Location = new System.Drawing.Point(12, 70);
             this.label8.Name = "label8";
             this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label8.Size = new System.Drawing.Size(285, 25);
@@ -155,7 +149,7 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(275, 492);
+            this.btnCancel.Location = new System.Drawing.Point(280, 535);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(102, 46);
             this.btnCancel.TabIndex = 15;
@@ -165,7 +159,7 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(383, 492);
+            this.btnSubmit.Location = new System.Drawing.Point(388, 535);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(113, 46);
             this.btnSubmit.TabIndex = 16;
@@ -175,10 +169,17 @@
             // 
             // txtDeadline
             // 
-            this.txtDeadline.Location = new System.Drawing.Point(275, 291);
+            this.txtDeadline.Location = new System.Drawing.Point(280, 334);
             this.txtDeadline.Name = "txtDeadline";
             this.txtDeadline.Size = new System.Drawing.Size(221, 20);
             this.txtDeadline.TabIndex = 19;
+            // 
+            // BTD_reportedDate
+            // 
+            this.BTD_reportedDate.Location = new System.Drawing.Point(280, 143);
+            this.BTD_reportedDate.Name = "BTD_reportedDate";
+            this.BTD_reportedDate.Size = new System.Drawing.Size(221, 20);
+            this.BTD_reportedDate.TabIndex = 22;
             // 
             // uc_MenuBar1
             // 
@@ -189,26 +190,36 @@
             this.uc_MenuBar1.TabIndex = 21;
             this.uc_MenuBar1.Window = null;
             // 
-            // BTD_reportedDate
+            // label1
             // 
-            this.BTD_reportedDate.Location = new System.Drawing.Point(275, 141);
-            this.BTD_reportedDate.Name = "BTD_reportedDate";
-            this.BTD_reportedDate.Size = new System.Drawing.Size(221, 20);
-            this.BTD_reportedDate.TabIndex = 22;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Reported date:";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(280, 184);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(221, 20);
+            this.txtSubject.TabIndex = 24;
             // 
             // AddIncident
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(803, 666);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BTD_reportedDate);
             this.Controls.Add(this.uc_MenuBar1);
             this.Controls.Add(this.txtDeadline);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtSubject);
             this.Controls.Add(this.cbPriority);
             this.Controls.Add(this.cbUser);
             this.Controls.Add(this.cbType);
@@ -237,12 +248,13 @@
         private System.Windows.Forms.ComboBox cbType;
         private System.Windows.Forms.ComboBox cbUser;
         private System.Windows.Forms.ComboBox cbPriority;
-        private System.Windows.Forms.TextBox txtSubject;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.TextBox txtDeadline;
         private Uc_MenuBar uc_MenuBar1;
         private System.Windows.Forms.DateTimePicker BTD_reportedDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubject;
     }
 }
